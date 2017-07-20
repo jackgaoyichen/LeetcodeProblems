64a658a6684e22a207967111cb6ad4912c001b5e
jackgaoyichen <jackgaoyichen@163.com>
2017-07-20 18:11:16 +0800

    add another ‘lexico’

--- Lexicographical Numbers 2.py
+++ Lexicographical Numbers 2.py
@@ -12,18 +12,22 @@ class Solution(object):
         return lexicalList
 
     def helper(self,cur, n, lexicalList):
+        len_n = len(str(n))
         if cur > n:
             return
         lexicalList.append(cur)
-        for i in range(0, 10):
-            if cur * 10 + i <= n:
-                self.helper(self,cur * 10 + i, n, lexicalList)
-            else:
-                break
+        for i in range(0,len_n):
+            len_j = 10**(i+1)
+            for j in range(0,len_j):
+                num = cur * len_j + j
+                if num > n:
+                    break
+                else:
+                    lexicalList.append(num)
 
 if __name__ == '__main__':
     sol = Solution
-    L = sol.lexicalOrder(sol,9)
+    L = sol.lexicalOrder(sol,1234)
     print(L)
     print(len(L))
 
